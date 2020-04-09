e462cec2115d1cbe159bf605c28ab26828b87774
Dmitry <dmitryvo83@gmail.com>
2020-04-09 23:45:57 +0300

    task3 completed

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let money = prompt('Your Monthly Budget?');
+let money = +prompt('Your Monthly Budget?', '');
 let time = Date.parse(prompt('Enter data as YYYY-MM-DD', 'YYYY-MM-DD'));
 
 
@@ -6,37 +6,38 @@ let appData = {
     budget: money,
     timeData: time,
     expenses: {},
-    /*expenses: {        
-         expenceItem: prompt('Enter mandatory expence item this Month'),
-         howMuchWllItCst: prompt('What would be the cost?'),
-    },*/
     optionalExpenses: {},
     income: [],
     savings: false,
 }
 
-debugger;
+for (let i = 0; i < 2; i++) {
+    while (true) {
+        let a = prompt('Enter mandatory expence item this Month', ''),
+            b = prompt('What would be the cost?', '');
 
-for (let i = 0; i < 2; i ++) {
-    let qtnMandatory = prompt('Enter mandatory expence item this Month');
-    let qtnHowMuch = prompt('What would be the cost?');
-    appData.expenses[qtnMandatory] = qtnHowMuch;
+        if ((typeof (a)) === 'string' && (typeof (a)) != null &&
+            (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
+            appData.expenses[a] = b;
+            console.log('done');   
+            break;     
+        } else {
+            alert('Please fill in the necessaty info');
+        }
+    }
 }
 
-// function getExpenses() {
-//     let a = appData.expenses.expenceItem;
-//     let b = appData.expenses.howMuchWllItCst;
-//     alert(`expenses: "${a}" : "${b}"`);
-// }
+appData.moneyPerDay = appData.budget / 30;
 
-// getExpenses();
+alert(`Everyday budget: ${Math.round(appData.moneyPerDay)}`);
 
+if (appData.moneyPerDay < 100) {
+    console.log("It's a minimum wage of life");        
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Middle class wages");    
+} else if (appData.moneyPerDay > 2000) {
+    console.log('You are reach bitch');
+} else {
+    console.log('Error');    
+}
 
-// function getDailyBudget() {
-//     oneDay = appData.budget / 30;
-//     alert(oneDay);
-// }
-
-// getDailyBudget();
-
-alert(appData.budget/30);
