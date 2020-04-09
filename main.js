3c8a650d91a66d7b9266f460ab1e2676ca256ffe
Dmitry <dmitryvo83@gmail.com>
2020-04-09 23:33:53 +0300

    Budget v3

--- main.js
+++ main.js
@@ -1,44 +1,42 @@
-let money = prompt('Your Budget');
+let money = prompt('Your Monthly Budget?');
 let time = Date.parse(prompt('Enter data as YYYY-MM-DD', 'YYYY-MM-DD'));
 
 
 let appData = {
     budget: money,
     timeData: time,
-    expenses: { 
-        expenceItem: prompt('Enter mandatory expence item this Month'),
-        howMuchWllItCst: prompt('What would be the cost?'),
-        // inResult: function() {        
-        //     return (`expenses: "${expenceItem}" : "${howMuchWllItCst}"`);
-        // }        
-    },    
-    optionalExpenses: {
-
-    },
+    expenses: {},
+    /*expenses: {        
+         expenceItem: prompt('Enter mandatory expence item this Month'),
+         howMuchWllItCst: prompt('What would be the cost?'),
+    },*/
+    optionalExpenses: {},
     income: [],
-    savings: false,    
+    savings: false,
 }
 
-// appData.expenses = function() {        
-//         return (`expenses: "${expenceItem}" : "${howMuchWllItCst}"`);
-//     }        
+debugger;
 
-// alert(appData.expenses);
-
-function getExpenses() {
-    let a = appData.expenses.expenceItem;
-    let b = appData.expenses.howMuchWllItCst;
-    alert(`expenses: "${a}" : "${b}"`);    
+for (let i = 0; i < 2; i ++) {
+    let qtnMandatory = prompt('Enter mandatory expence item this Month');
+    let qtnHowMuch = prompt('What would be the cost?');
+    appData.expenses[qtnMandatory] = qtnHowMuch;
 }
 
-getExpenses();
+// function getExpenses() {
+//     let a = appData.expenses.expenceItem;
+//     let b = appData.expenses.howMuchWllItCst;
+//     alert(`expenses: "${a}" : "${b}"`);
+// }
 
-function getDailyBudget() {
-    oneDay = appData.budget/30,
-    alert(oneDay);         
-}
-
-getDailyBudget();
-
+// getExpenses();
 
 
+// function getDailyBudget() {
+//     oneDay = appData.budget / 30;
+//     alert(oneDay);
+// }
+
+// getDailyBudget();
+
+alert(appData.budget/30);
