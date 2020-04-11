532273c87338558e1217dd2cb36b8afad1b6abd3
Dmitry <dmitryvo83@gmail.com>
2020-04-11 12:31:22 +0300

    task 4 v1

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-let money = +prompt('Your Monthly Budget?', '');
-let time = Date.parse(prompt('Enter data as YYYY-MM-DD', 'YYYY-MM-DD'));
+let money, time;
+
+function start() {
+    money = +prompt('Your Monthly Budget?', '');
+    time = prompt('Enter data as YYYY-MM-DD', 'YYYY-MM-DD');
+    while (isNaN(money) || money == '' || money == null) {
+        money = +prompt('Your Monthly Budget?', '');
+    }
+}
+
+start();
 
 
 let appData = {
@@ -8,36 +17,50 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false,
-}
+    savings: true,
+};
 
-for (let i = 0; i < 2; i++) {
-    while (true) {
-        let a = prompt('Enter mandatory expence item this Month', ''),
-            b = prompt('What would be the cost?', '');
+function chooseExpenses() {
+    for (let i = 0; i < 2; i++) {
+        while (true) {
+            let a = prompt('Enter mandatory expence item this Month', ''),
+                b = prompt('What would be the cost?', '');
 
-        if ((typeof (a)) === 'string' && (typeof (a)) != null &&
-            (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
-            appData.expenses[a] = b;
-            console.log('done');   
-            break;     
-        } else {
-            alert('Please fill in the necessaty info');
+            if ((typeof (a)) === 'string' && (typeof (a)) != null &&
+                (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
+                appData.expenses[a] = b;
+                console.log('done');
+                break;
+            } else {
+                alert('Please fill in the necessaty info');
+            }
         }
     }
 }
 
-appData.moneyPerDay = appData.budget / 30;
+chooseExpenses();
 
-alert(`Everyday budget: ${Math.round(appData.moneyPerDay)}`);
+appData.moneyPerDay = (appData.budget / 30).toFixed();
+
+alert(`Everyday budget: ${appData.moneyPerDay}`);
 
 if (appData.moneyPerDay < 100) {
-    console.log("It's a minimum wage of life");        
+    console.log("It's a minimum wage of life");
 } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log("Middle class wages");    
+    console.log("Middle class wages");
 } else if (appData.moneyPerDay > 2000) {
     console.log('You are reach bitch');
 } else {
-    console.log('Error');    
+    console.log('Error');
 }
 
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt('Please type in the total amount of your savings'),
+            percent = +prompt('Please type in the required percentage');
+        appData.monthIncome = (save / 100 / 12) * percent;
+        alert('Monthley deposit income is: ' + appData.monthIncome);
+    }
+}
+
+checkSavings();
